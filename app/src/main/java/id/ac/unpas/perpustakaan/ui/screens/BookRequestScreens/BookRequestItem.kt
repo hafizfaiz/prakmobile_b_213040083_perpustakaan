1a629b0882e765585cf022cbdc5497903d4104d1
Anggakusuma74 <kusumaangga719@gmail.com>
2024-06-05 12:32:06 +0700

    Memperbaiki Icon di BookRequestItem

--- app/src/main/java/id/ac/unpas/perpustakaan/ui/screens/BookRequestScreens/BookRequestItem.kt
+++ app/src/main/java/id/ac/unpas/perpustakaan/ui/screens/BookRequestScreens/BookRequestItem.kt
@@ -1,8 +1,10 @@
 package id.ac.unpas.perpustakaan.ui.screens.BookRequestScreens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,11 +20,11 @@ fun BookRequestItem(item: BookRequest, onEditClick: (String) -> Unit, onDeleteCl
         Text(modifier = Modifier.weight(3f), text = item.start_date)
         Text(modifier = Modifier.weight(3f), text = item.end_date)
         Text(modifier = Modifier.weight(3f), text = item.status)
-        Button(modifier = Modifier.weight(1.5f), onClick = { onEditClick(item.id) }) {
-            Image(painterResource(id = R.drawable.baseline_edit_24), contentDescription = "Edit")
-        }
-        Button(modifier = Modifier.weight(1.5f), onClick = { onDeleteClick(item.id) }) {
-            Image(painterResource(id = R.drawable.baseline_delete_24), contentDescription = "Delete")
-        }
+        Icon(painterResource(id = R.drawable.baseline_edit_24), "Edit", modifier = Modifier.weight(1.5f).clickable {
+            onEditClick(item.id)
+        })
+        Icon(painterResource(id = R.drawable.baseline_delete_24), "Delete", modifier = Modifier.weight(1.5f).clickable {
+            onDeleteClick(item.id)
+        })
     }
 }
