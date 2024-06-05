4becbcdd7c0d37c26c314c84a9215423726804b4
Anggakusuma74 <kusumaangga719@gmail.com>
2024-06-05 20:29:40 +0700

    Update fitur Hapus di ListBookScreen, ListMembershipScreen dan ListBookRequestScreen

--- app/src/main/java/id/ac/unpas/perpustakaan/ui/screens/BookRequestScreens/ListBookRequestScreen.kt
+++ app/src/main/java/id/ac/unpas/perpustakaan/ui/screens/BookRequestScreens/ListBookRequestScreen.kt
@@ -3,6 +3,8 @@ package id.ac.unpas.perpustakaan.ui.screens.BookRequestScreens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,6 +28,15 @@ fun ListBookRequestScreen(modifier: Modifier = Modifier, onClick: (String) -> Un
 
     val list: List<BookRequest> by viewModel.bookRequests.observeAsState(listOf())
     val title = remember { mutableStateOf("BOOK_REQUEST") }
+    val openDialog = remember {
+        mutableStateOf(false)
+    }
+    val activeId = remember {
+        mutableStateOf("")
+    }
+    val deleting = remember {
+        mutableStateOf(false)
+    }
 
     Column(modifier = modifier.fillMaxWidth()) {
         Text(text = title.value, modifier = Modifier.fillMaxWidth())
@@ -35,12 +46,40 @@ fun ListBookRequestScreen(modifier: Modifier = Modifier, onClick: (String) -> Un
                 BookRequestItem(item = item, onEditClick = { id ->
                     onClick(id)
                 }, onDeleteClick = { id ->
-                    scope.launch {
-                        viewModel.delete(id)
-                    }
+                    deleting.value = true
+                    activeId.value = id
+                    openDialog.value = true
                 })
             }
         }
+    }
+
+    if (openDialog.value) {
+        AlertDialog(onDismissRequest = {
+            openDialog.value = false },
+            title = {
+                Text(text = "Konfirmasi")
+            },
+            text = {
+                Text(text = "Apakah anda yakin ingin menghapus data ini?")
+            },
+            confirmButton = { Button(onClick = {
+                scope.launch {
+                    viewModel.delete(activeId.value)
+                }
+                openDialog.value = false
+            }) {
+                Text(text = "Ya")
+            }
+            },
+            dismissButton = {
+                Button(onClick = {
+                    openDialog.value = false
+                }) {
+                    Text(text = "Tidak")
+                }
+            }
+        )
     }
 
     viewModel.isLoading.observe(LocalLifecycleOwner.current) {
--- app/src/main/java/id/ac/unpas/perpustakaan/ui/screens/MembershipScreens/ListMembershipScreen.kt
+++ app/src/main/java/id/ac/unpas/perpustakaan/ui/screens/MembershipScreens/ListMembershipScreen.kt
@@ -3,6 +3,8 @@ package id.ac.unpas.perpustakaan.ui.screens.MembershipScreens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,6 +26,15 @@ fun ListMembershipScreen(modifier: Modifier = Modifier, onClick: (String) -> Uni
 
     val list: List<Membership> by viewModel.memberships.observeAsState(listOf())
     val title = remember { mutableStateOf("Membership") }
+    val openDialog = remember {
+        mutableStateOf(false)
+    }
+    val activeId = remember {
+        mutableStateOf("")
+    }
+    val deleting = remember {
+        mutableStateOf(false)
+    }
 
     Column(modifier = modifier.fillMaxWidth()) {
         Text(text = title.value, modifier = Modifier.fillMaxWidth())
@@ -33,12 +44,40 @@ fun ListMembershipScreen(modifier: Modifier = Modifier, onClick: (String) -> Uni
                 MembershipItem(item = item, onEditClick = { id ->
                     onClick(id)
                 }, onDeleteClick = { id ->
-                    scope.launch {
-                        viewModel.delete(id)
-                    }
+                    deleting.value = true
+                    activeId.value = id
+                    openDialog.value = true
                 })
             }
         }
+    }
+
+    if (openDialog.value) {
+        AlertDialog(onDismissRequest = {
+            openDialog.value = false },
+            title = {
+                Text(text = "Konfirmasi")
+            },
+            text = {
+                Text(text = "Apakah anda yakin ingin menghapus data ini?")
+            },
+            confirmButton = { Button(onClick = {
+                scope.launch {
+                    viewModel.delete(activeId.value)
+                }
+                openDialog.value = false
+            }) {
+                Text(text = "Ya")
+            }
+            },
+            dismissButton = {
+                Button(onClick = {
+                    openDialog.value = false
+                }) {
+                    Text(text = "Tidak")
+                }
+            }
+        )
     }
 
     viewModel.isLoading.observe(LocalLifecycleOwner.current) {
