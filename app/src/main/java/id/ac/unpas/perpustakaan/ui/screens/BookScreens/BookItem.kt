e0aaa9160102e31188d5e3655793e7a0b173afc5
syahidanfitrah <syahidanfitrah9@gmail.com>
2024-06-06 00:48:52 +0700

    Memperbaiki tampilan data list buku

--- app/src/main/java/id/ac/unpas/perpustakaan/ui/screens/BookScreens/BookItem.kt
+++ app/src/main/java/id/ac/unpas/perpustakaan/ui/screens/BookScreens/BookItem.kt
@@ -1,29 +1,96 @@
 package id.ac.unpas.perpustakaan.ui.screens.BookScreens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import id.ac.unpas.perpustakaan.R
 import id.ac.unpas.perpustakaan.models.Book
 
 @Composable
 fun BookItem(item: Book, onEditClick: (String) -> Unit, onDeleteClick: (String) -> Unit) {
-    Row {
-        Text(modifier = Modifier.weight(3f), text = item.title)
-        Text(modifier = Modifier.weight(3f), text = item.author)
-        Text(modifier = Modifier.weight(3f), text = item.released_date)
-        Text(modifier = Modifier.weight(3f), text = item.stock)
-        Icon(painterResource(id = R.drawable.baseline_edit_24), "Edit", modifier = Modifier.weight(1.5f).clickable {
-            onEditClick(item.id)
-        })
-        Icon(painterResource(id = R.drawable.baseline_delete_24), "Delete", modifier = Modifier.weight(1.5f).clickable {
-            onDeleteClick(item.id)
-        })
+    Box(
+        modifier = Modifier
+            .padding(8.dp)
+            .border(1.dp, Color.Black, shape = RoundedCornerShape(15.dp))
+    ) {
+        Row (modifier = Modifier.padding(7.dp), verticalAlignment = Alignment.CenterVertically){
+            Image(
+                painter = painterResource(id = R.drawable.baseline_book2_24),
+                contentDescription = null,
+                modifier = Modifier.size(100.dp)
+            )
+            Column(modifier = Modifier.weight(0.5f)) {
+                Text(text = "Nama Buku")
+                Text(text = "Penulis")
+                Text(text = "Tahun Rilis")
+                Text(text = "Jumlah Stock")
+            }
+            Column(modifier = Modifier.weight(0.1f)) {
+                Text(text = ":")
+                Text(text = ":")
+                Text(text = ":")
+                Text(text = ":")
+            }
+            Column(modifier = Modifier.weight(0.5f)) {
+                Text(text = item.title)
+                Text(text = item.author)
+                Text(text = item.released_date)
+                Text(text = item.stock)
+            }
+            Row(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(start = 8.dp)
+            ) {
+                Icon(
+                    painterResource(id = R.drawable.baseline_edit_24),
+                    "Edit",
+                    modifier = Modifier
+                        .clickable {
+                            onEditClick(item.id)
+                        })
+                Icon(
+                    painterResource(id = R.drawable.baseline_delete_24),
+                    "Delete",
+                    modifier = Modifier
+                        .clickable {
+                            onDeleteClick(item.id)
+                        })
+            }
+        }
     }
+//    Row {
+//        Text(modifier = Modifier.weight(3f), text = item.title)
+//        Text(modifier = Modifier.weight(3f), text = item.author)
+//        Text(modifier = Modifier.weight(3f), text = item.released_date)
+//        Text(modifier = Modifier.weight(3f), text = item.stock)
+//        Icon(painterResource(id = R.drawable.baseline_edit_24), "Edit", modifier = Modifier
+//            .weight(1.5f)
+//            .clickable {
+//                onEditClick(item.id)
+//            })
+//        Icon(painterResource(id = R.drawable.baseline_delete_24), "Delete", modifier = Modifier
+//            .weight(1.5f)
+//            .clickable {
+//                onDeleteClick(item.id)
+//            })
+//    }
 }
--- app/src/main/java/id/ac/unpas/perpustakaan/ui/screens/BookScreens/ListBookScreen.kt
+++ app/src/main/java/id/ac/unpas/perpustakaan/ui/screens/BookScreens/ListBookScreen.kt
@@ -2,9 +2,9 @@ package id.ac.unpas.perpustakaan.ui.screens.BookScreens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import id.ac.unpas.perpustakaan.models.Book
 import id.ac.unpas.perpustakaan.ui.screens.BookScreens.BookItem
@@ -28,58 +29,24 @@ fun ListBookScreen(modifier: Modifier = Modifier, onClick: (String) -> Unit) {
 
     val list: List<Book> by viewModel.books.observeAsState(listOf())
     val title = remember { mutableStateOf("BOOK") }
-    val openDialog = remember {
-        mutableStateOf(false)
-    }
-    val activeId = remember {
-        mutableStateOf("")
-    }
-    val deleting = remember {
-        mutableStateOf(false)
-    }
 
     Column(modifier = modifier.fillMaxWidth()) {
         Text(text = title.value, modifier = Modifier.fillMaxWidth())
-        LazyColumn(modifier = Modifier.fillMaxWidth()) {
+        LazyColumn(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)) {
             items(list.size) { index ->
                 val item = list[index]
                 BookItem(item = item, onEditClick = { id ->
                     onClick(id)
                 }, onDeleteClick = { id ->
-                    deleting.value = true
-                    activeId.value = id
-                    openDialog.value = true
+                    scope.launch {
+                        viewModel.delete(id)
+                    }
                 })
+                Divider()
             }
         }
-    }
-
-    if (openDialog.value) {
-        AlertDialog(onDismissRequest = {
-            openDialog.value = false },
-            title = {
-                Text(text = "Konfirmasi")
-            },
-            text = {
-                Text(text = "Apakah anda yakin ingin menghapus data ini?")
-            },
-            confirmButton = { Button(onClick = {
-                scope.launch {
-                    viewModel.delete(activeId.value)
-                }
-                openDialog.value = false
-            }) {
-                Text(text = "Ya")
-                }
-            },
-            dismissButton = {
-                Button(onClick = {
-                    openDialog.value = false
-                }) {
-                    Text(text = "Tidak")
-                }
-            }
-        )
     }
 
     viewModel.isLoading.observe(LocalLifecycleOwner.current) {
